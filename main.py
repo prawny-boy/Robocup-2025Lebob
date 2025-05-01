e48584780d1ec4e36bba4c41dba4922f68277b2f
AaronMisc <aaron.miscellaneous@outlook.com>
2025-05-01 17:19:00 +0800

    add ultra sonic sensor into imports correct drivebase wheel information add arm motor remove movement change color to reflection of the colour sensors print the left and right reflections note: only for debugging

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.hubs import PrimeHub
 from pybricks.robotics import DriveBase
 from pybricks.parameters import Port, Color, Axis, Direction, Button, Stop
 
-DRIVEBASE_WHEEL_DIAMETER = 0
-DRIVEBASE_AXLE_TRACK = 0
+DRIVEBASE_WHEEL_DIAMETER = 56
+DRIVEBASE_AXLE_TRACK = 112
 ROBOT_SPEED = 500
 ROBOT_ACCELERATION = 750
 ROBOT_TURN_RATE = 750
 ROBOT_TURN_ACCELERATION = 3000
 
+ports = {
+    "left_drive": Port.E,
+    "right_drive": Port.C,
+    "colour_sensor_left": Port.D,
+    "colour_sensor_right": Port.A,
+    "ultrasonic_sensor": Port.B,
+    "arm_motor": Port.F,
+}
+
 class Robot:
     def __init__(self):
         self.hub = PrimeHub(Axis.Z, Axis.X)
-        self.left_drive = Motor(Port.A)
-        self.right_drive = Motor(Port.B)
+        self.left_drive = Motor(ports["left_drive"])
+        self.right_drive = Motor(ports["right_drive"])
 
-        self.colour_sensor_left = ColorSensor(Port.C)
-        self.colour_sensor_right = ColorSensor(Port.D)
+        self.colour_sensor_left = ColorSensor(ports["colour_sensor_left"])
+        self.colour_sensor_right = ColorSensor(ports["colour_sensor_right"])
+        self.ultrasonic_sensor = UltrasonicSensor(ports["ultrasonic_sensor"])
+        self.arm_motor = Motor(ports["arm_motor"])
 
         self.drivebase = DriveBase(self.left_drive, self.right_drive, DRIVEBASE_WHEEL_DIAMETER, DRIVEBASE_AXLE_TRACK)
         self.drivebase.use_gyro(False)
@@ -47,16 +58,17 @@ def main():
 
     while True:
         # Sense colours below
-        colour_left = robot.colour_sensor_left.color()
-        colour_right = robot.colour_sensor_right.color()
+        reflection_left = robot.colour_sensor_left.reflection()
+        reflection_right = robot.colour_sensor_right.reflection()
+        print(reflection_left, reflection_right)
 
         # Turn if needed
-        if colour_left == Color.BLACK and colour_right == Color.WHITE: # Left
-            robot.turn_in_degrees(-1)
-        elif colour_left == Color.WHITE and colour_right == Color.BLACK: # Right
-            robot.turn_in_degrees(1)
-        else: # Forward
-            robot.move_forward(1)
+        if False:
+            if colour_left == Color.BLACK and colour_right == Color.WHITE: # Left
+                robot.turn_in_degrees(-1)
+            elif colour_left == Color.WHITE and colour_right == Color.BLACK: # Right
+                robot.turn_in_degrees(1)
+            else: # Forward
+                robot.move_forward(1)
 
-if __name__ == "main":
-    main()
+main()
