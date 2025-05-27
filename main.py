0261250f60718bd30635d14e144154325e14f287
andre <andre.perth.2010@gmail.com>
2025-05-27 12:24:48 +0800

    Fix DriveBase.settings() to use positional args
    DriveBase.settings() was using keyword arguments which worked on
    some laptops but failed on mine, probablh=y differences in
    Python version or pybricks runtime. Also fixed some stupid formatting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pybricks.hubs import PrimeHub
 from pybricks.robotics import DriveBase
 from pybricks.parameters import Port, Color, Axis, Direction, Button, Stop
 
+
 DRIVEBASE_WHEEL_DIAMETER = 56
 DRIVEBASE_AXLE_TRACK = 112
 ROBOT_SPEED = 500
@@ -21,6 +22,7 @@ ports = {
     "arm_motor": Port.F,
 }
 
+
 class Robot:
     def __init__(self):
         self.hub = PrimeHub(Axis.Z, Axis.X)
@@ -35,46 +37,41 @@ class Robot:
         self.drivebase = DriveBase(self.left_drive, self.right_drive, DRIVEBASE_WHEEL_DIAMETER, DRIVEBASE_AXLE_TRACK)
         self.drivebase.use_gyro(False)
         self.drivebase.settings(
-            straight_speed=ROBOT_SPEED, 
-            straight_acceleration=ROBOT_ACCELERATION, 
-            turn_rate=ROBOT_TURN_RATE, 
-            turn_acceleration=ROBOT_TURN_ACCELERATION
+            ROBOT_SPEED,
+            ROBOT_ACCELERATION,
+            ROBOT_TURN_RATE,
+            ROBOT_TURN_ACCELERATION
         )
 
         self.robot_state = "obstacle"
-    
+
     def turn_in_degrees(self, degrees=ROBOT_TURNING_DEGREES):
-        """Turns in degrees."""
         self.drivebase.curve(25, degrees, Stop.COAST, False)
-    
+
     def short_turn_in_degrees(self, degrees):
         self.drivebase.turn(degrees)
-    
+
     def move_forward(self, distance, speed=None):
-        """Moves forward in mm."""
         if speed is not None:
             self.set_speed(speed)
         self.drivebase.straight(distance)
-    
+
     def start_motors(self, left_speed, right_speed):
         if not self.left_drive.speed() == left_speed:
             self.left_drive.run(left_speed)
         if not self.right_drive.speed() == right_speed:
             self.right_drive.run(right_speed)
-    
+
     def stop_motors(self):
         self.left_drive.stop()
         self.right_drive.stop()
 
     def set_speed(self, speed):
-        """Sets the speed of the robot in drivebase settings."""
         if speed == 0:
             speed = ROBOT_SPEED
-        self.drivebase.settings(straight_speed=speed)
+        self.drivebase.settings(speed, ROBOT_ACCELERATION, ROBOT_TURN_RATE, ROBOT_TURN_ACCELERATION)
 
     def information_to_color(self, information):
-        # if information["reflection"] == 100:
-        #     return Color.GRAY
         if information["color"] == Color.WHITE:
             return Color.WHITE
         elif information["color"] == Color.GREEN and information["hsv"].h > 135 and information["hsv"].h < 165:
@@ -83,13 +80,13 @@ class Robot:
             return Color.BLUE
         elif information["color"] == Color.RED and information["hsv"].s > 85:
             return Color.RED
-        elif information["color"] == Color.RED: # information["hsv"].s <= 85
+        elif information["color"] == Color.RED:
             return Color.ORANGE
         elif information["hsv"].s < 20 and information["reflection"] < 30 and information["reflection"] > 3:
             return Color.BLACK
-        else: 
+        else:
             return Color.NONE
-    
+
     def get_colors(self):
         self.left_color_sensor_information = {
             "reflection": self.color_sensor_left.reflection(),
@@ -117,33 +114,24 @@ class Robot:
         self.get_colors()
         self.ultrasonic = self.ultrasonic_sensor.distance()
 
-        # check for obstacle
         if self.ultrasonic < 80:
             self.robot_state = "obstacle"
             return
 
-        # forward
         if self.left_color == Color.WHITE and self.right_color == Color.WHITE:
             self.robot_state = "straight"
-
-        # black line following
         elif self.left_color == Color.BLACK and self.robot_state != "left":
-            self.robot_state = "new left" # new will mean that it will stop then turn
+            self.robot_state = "new left"
         elif self.right_color == Color.BLACK and self.robot_state != "right":
             self.robot_state = "new right"
-
-        # green
         elif self.left_color == Color.GREEN:
             self.robot_state = "green left"
         elif self.right_color == Color.GREEN:
             self.robot_state = "green right"
-
-        # nothing
         else:
             self.robot_state = "stop"
 
     def move(self):
-        # obstackle
         if self.robot_state == "obstacle":
             self.stop_motors()
             self.short_turn_in_degrees(90)
@@ -155,47 +143,42 @@ class Robot:
             self.turn_in_degrees(45)
             self.robot_state = "straight"
 
-        # straight
         if self.robot_state == "straight":
             self.start_motors(ROBOT_MOVE_SPEED, ROBOT_MOVE_SPEED)
-
-        elif self.robot_state == "new left": # this will stop, then set it to left
+        elif self.robot_state == "new left":
             self.stop_motors()
             self.turn_in_degrees(-ROBOT_TURNING_DEGREES)
             self.robot_state = "left"
-        elif self.robot_state == "left": # left, no stopping
+        elif self.robot_state == "left":
             self.turn_in_degrees(-ROBOT_TURNING_DEGREES)
-
-        elif self.robot_state == "new right": # this will stop, then set it to right
+        elif self.robot_state == "new right":
             self.stop_motors()
             self.turn_in_degrees(ROBOT_TURNING_DEGREES)
             self.robot_state = "right"
-        elif self.robot_state == "right": # right, no stopping
+        elif self.robot_state == "right":
             self.turn_in_degrees(ROBOT_TURNING_DEGREES)
-        
-        # green
         elif self.robot_state == "green left":
             self.stop_motors()
             self.turn_green("left")
         elif self.robot_state == "green right":
             self.stop_motors()
             self.turn_green("right")
-
-        # stop
         elif self.robot_state == "stop":
             self.stop_motors()
-        
-    def debug(self):       
+
+    def debug(self):
         print(self.left_color_sensor_information, self.left_color)
-    
+
     def run(self):
         while True:
             self.update()
             self.move()
             self.debug()
 
+
 def main():
     robot = Robot()
     robot.run()
+
 
 main()
