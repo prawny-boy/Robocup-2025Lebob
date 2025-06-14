a1f31f2de13e73a4cd16c88f055e6f0017bd13d2
AaronMisc <aaron.miscellaneous@outlook.com>
2025-06-14 13:31:48 +0800

    feat(main): add ALLOW_YELLOW setting so the user can turn of shortcut following if they want
    feat(rotate arm): make the rotate arm function use Stop.BRAKE instead of Stop.COAST for more accuracy
    
    feat(information to color): add gray color detection
    
    feat(main): add spill zone logic, NOT TESTED!!!

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pybricks.hubs import PrimeHub
 from pybricks.robotics import DriveBase
 from pybricks.parameters import Port, Color, Axis, Direction, Button, Stop
 
+ALLOW_YELLOW = False    
+
 # --- CONSTANTS ---
 CONSTANTS = {
     "DRIVEBASE_WHEEL_DIAMETER": 56,
@@ -100,7 +102,7 @@ class Robot:
 
     def rotate_arm(self, degrees):
         """Rotate the arm's motor based on degrees."""
-        self.arm_motor.run_angle(CONSTANTS["ARM_MOVE_SPEED"], degrees, Stop.COAST, False)
+        self.arm_motor.run_angle(CONSTANTS["ARM_MOVE_SPEED"], degrees, Stop.BRAKE, False)
 
     def set_speed(self, speed):
         """Set the speed of the robot in drivebase settings."""
@@ -110,9 +112,11 @@ class Robot:
 
     def information_to_color(self, information):
         """Convert color sensor information to a color."""
-        if information["color"] == Color.WHITE:
+        if information["reflection"] == 100:
+            return Color.GRAY
+        elif information["color"] == Color.WHITE:
             return Color.WHITE
-        elif information["hsv"].h < 67 and information["hsv"].h > 45:
+        elif information["hsv"].h < 67 and information["hsv"].h > 45 and ALLOW_YELLOW:
             return Color.YELLOW
         elif information["color"] == Color.GREEN and 135 < information["hsv"].h < 165:
             return Color.GREEN
@@ -153,8 +157,12 @@ class Robot:
         self.drivebase.curve(CONSTANTS["CURVE_RADIUS_GREEN"], degrees, Stop.COAST, True)
 
     def follow_line(self):
-        reflection_difference = self.left_color_sensor_information["reflection"] - self.right_color_sensor_information["reflection"]
-        turn_rate = max(min(3.2 * reflection_difference, CONSTANTS["MAX_TURN_RATE"]), -CONSTANTS["MAX_TURN_RATE"])
+        if not self.on_inverted:
+            reflection_difference = self.left_color_sensor_information["reflection"] - self.right_color_sensor_information["reflection"]
+        else:
+            reflection_difference = self.right_color_sensor_information["reflection"] - self.left_color_sensor_information["reflection"]
+        
+        turn_rate = max(min(3.3 * reflection_difference, CONSTANTS["MAX_TURN_RATE"]), -CONSTANTS["MAX_TURN_RATE"])
         self.drivebase.drive(CONSTANTS["MOVE_SPEED"], turn_rate)
 
         while not self.drivebase.done(): # To check if both are black
@@ -170,6 +178,66 @@ class Robot:
             self.turn_in_degrees(CONSTANTS["TURN_YELLOW_DEGREES"])
         else:
             self.drivebase.drive(CONSTANTS["MOVE_SPEED"], 0)
+
+    def turn_and_detect_ultrasonic(self, degrees):
+        "Turn the number of degrees, and meanwhile, find the lowest ultrasonic and return the lowest ultrasonic."
+        lowest_ultrasonic = 2000
+        lowest_ultrasonic_angle = 0
+
+        self.drivebase.reset()
+        self.sharp_turn_in_degrees(degrees)
+
+        while not self.drivebase.done():
+            new_ultrasonic = self.ultrasonic_sensor.distance()
+
+            if new_ultrasonic < lowest_ultrasonic:
+                lowest_ultrasonic = new_ultrasonic
+                lowest_ultrasonic_angle = self.drivebase.angle()
+        
+        return lowest_ultrasonic, lowest_ultrasonic_angle
+    
+    def gray_ending(self):
+        self.move_forward(30)
+
+        self.stop_motors()
+        
+        lowest_ultrasonic = 2000
+        lowest_ultrasonic_angle = 0
+
+        angles = [-90, 180, 90]
+        for i, angle in enumerate(angles):
+            new_ultrasonic, new_ultrasonic_angle = self.turn_and_detect_ultrasonic(angle)
+
+            if 1 != 0:
+                original_angle = sum(angles[:i])
+            else:
+                original_angle = 0
+            
+            new_ultrasonic_angle += original_angle
+
+            if new_ultrasonic < lowest_ultrasonic:
+                lowest_ultrasonic = new_ultrasonic
+                lowest_ultrasonic_angle = new_ultrasonic_angle
+        
+        if lowest_ultrasonic != 2000: # Sensed the can
+            self.drivebase.reset()
+            self.sharp_turn_in_degrees(lowest_ultrasonic_angle)
+            self.move_forward(lowest_ultrasonic - 10)
+            self.rotate_arm(180)
+
+            self.sharp_turn_in_degrees(180)
+            self.move_forward(lowest_ultrasonic - 10)
+
+            self.sharp_turn_in_degrees(-lowest_ultrasonic_angle)
+        
+        self.move_forward(30)
+
+        # Back at the gray here
+
+        self.move_forward(50)
+        self.sharp_turn_in_degrees(60)
+        self.rotate_arm(-180)
+        self.sharp_turn_in_degrees(-60)
 
     def avoid_obstacle(self):
         self.stop_motors()
@@ -192,6 +260,10 @@ class Robot:
 
         self.previous_state = self.robot_state
 
+        if (self.left_color == Color.GRAY and self.right_color == Color.GRAY) or (self.left_color == Color.GREEN and self.right_color == Color.GREEN):
+            self.robot_state = "gray"
+            return
+        
         if self.left_color == Color.BLACK and self.right_color == Color.BLACK:
             self.black_counter += 1
         else:
@@ -204,11 +276,11 @@ class Robot:
         
         # Check for obstacle
         if self.ultrasonic < CONSTANTS["ULTRASONIC_THRESHOLD"]:
-            # self.robot_state = "obstacle"
+            self.robot_state = "obstacle"
             return # Exit update early if obstacle detected
 
         # Shortcut / Yellow
-        elif self.left_color == Color.YELLOW or self.right_color == Color.YELLOW:
+        elif ALLOW_YELLOW and (self.left_color == Color.YELLOW or self.right_color == Color.YELLOW): # CURRENTLY DISABLED
             self.robot_state = "yellow line"
             if self.robot_state != self.previous_state and self.iteration_count > self.shortcut_cooldown_end:
                 self.shortcut_cooldown_end = self.iteration_count + 1000
@@ -220,8 +292,9 @@ class Robot:
         
         # Line
         elif self.iteration_count > self.shortcut_cooldown_end and \
-            self.left_color in [Color.WHITE, Color.BLACK] and \
-            self.right_color in [Color.WHITE, Color.BLACK]:
+            self.left_color in [Color.WHITE, Color.BLACK, Color.GRAY] and \
+            self.right_color in [Color.WHITE, Color.BLACK, Color.GRAY] and \
+            not (self.left_color == self.right_color and self.left_color == Color.GRAY): # Both not gray
             self.robot_state = "line"
 
         # Turn / Green
@@ -237,8 +310,12 @@ class Robot:
 
     def move(self):
         """Move the robot based on its current state."""
+        # Gray
+        if self.robot_state == "gray":
+            self.gray_ending()
+        
         # Obstacle
-        if self.robot_state == "obstacle":
+        elif self.robot_state == "obstacle":
             self.avoid_obstacle()
 
         # ShortcutYellow
@@ -264,17 +341,18 @@ class Robot:
     def debug(self):
         """Print debug text."""
         # print(self.robot_state)
-        print(self.left_color_sensor_information, self.left_color)
+        # print(self.left_color_sensor_information, self.left_color)
         # print(self.right_color_sensor_information, self.right_color)
         # print(self.left_color, self.right_color)
         # print(self.iteration_count)
+        print(self.ultrasonic)
 
     def run(self):
         self.battery_display()
         while True:
             self.iteration_count += 1 # Increment the counter at the start of each loop
             self.update()
-            self.move()
+            # self.move()
             self.debug()
 
 def main():
